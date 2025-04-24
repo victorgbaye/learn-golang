f71f8dccc2151bbcf49eb5e0d5a7a04dbea086cb
tomiwadotJS <victorgbayedev@gmail.com>
2025-04-24 13:59:33 +0100

    array and slices

--- array/sum.go
+++ array/sum.go
@@ -1,8 +1,8 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func Sum(numbers [5]int) int{
+func Sum(numbers []int) int{
 	sum := 0
 	for _, num := range numbers {
 		sum += num
@@ -11,7 +11,17 @@ func Sum(numbers [5]int) int{
 
 }
 
-func main()  {
-	num := [5]int{1,2,3,4,5}
-	fmt.Println(Sum(num))
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
+// func main()  {
+// 	num := []int{1,2,3,4,5}
+// 	fmt.Println(Sum(num))
+// }
--- array/sum_test.go
+++ array/sum_test.go
@@ -1,13 +1,37 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	var numbers = [5]int{1,2,3,4,5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("collectionn of 5 numbers", func(t *testing.T){
+		var numbers = []int{1,2,3,4,5}
+		got := Sum(numbers)
+		want := 15
+	
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		var number = []int{1,2,3}
+		got := Sum(number)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want want %d given, %v", got, want, number)
+		}
+
+	})
+
+	t.Run("sum all slices",func(t *testing.T) {
+		got := SumAll([]int{1, 4}, []int{2,3})
+		want := []int{3, 7}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
