8c3516ad28866ccd561d2a76e2e3b01d3d27b38f
tomiwadotJS <victorgbayedev@gmail.com>
2025-04-24 11:21:11 +0100

    multiple if statements

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 const englishHelloPrefix string = "Hello, "
+const spanishHelloPrefix string = "Hola, "
+const frenchHelloPrefix string = "Bonjour, "
 
-func Hello(name string) string{
+func Hello(name, language string) string{
 	if name == ""{
 		return englishHelloPrefix + "world"
+	}
+	
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	if language == "French" {
+		return frenchHelloPrefix + name
+	}
+	if language == "English" {
+		return englishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main () {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "English"))
 }
--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -4,19 +4,26 @@ import "testing"
 
 func TestHello (t *testing.T){
 	t.Run("Saying hello world to people", func(t *testing.T)  {
-		got:= Hello("Chris")
+		got:= Hello("Chris", "English")
 		want:="Hello, Chris"
-	
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say hello world when an empty string is supplied", func(t *testing.T){
-		got:= Hello("")
+		got:= Hello("", "English")
 		want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("In spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string){
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
